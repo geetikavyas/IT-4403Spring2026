b07a24a10a520e19d3e7ecf486c3b358cfe02b68
geetikavyas <168227711+geetikavyas@users.noreply.github.com>
2026-02-20 23:22:04 -0500

    Update app.js

--- milestone2/js/app.js
+++ milestone2/js/app.js
@@ -1,78 +1,95 @@
 $(document).ready(function () {
 
-    var apiKey = "AIzaSyAaXzebLvsKc4q66a4Ex0cux_FU-lrAalY";
-    var allBooks = [];
-    var resultsPerPage = 10;
+  var apiKey = "AIzaSyAaXzebLvsKc4q66a4Ex0cux_FU-lrAalY";  // keep your real key here
+  var allBooks = [];
+  var resultsPerPage = 10;
 
-    $("#btnSearch").click(function () {
+  $("#btnSearch").click(function () {
 
-        var searchTerm = $("#searchTerm").val().trim();
-        if (searchTerm === "") {
-            alert("Please enter a search term.");
-            return;
-        }
+    var searchTerm = $("#searchTerm").val().trim();
 
-        var apiUrl =
-            "https://www.googleapis.com/books/v1/volumes?q=" +
-            encodeURIComponent(searchTerm) +
-            "&maxResults=40" +
-            "&key=" + apiKey;
-
-        $.getJSON(apiUrl, function (data) {
-
-            allBooks = data.items || [];
-            createPagination();
-            displayPage(1);
-
-        });
-
-    });
-
-    function createPagination() {
-        $("#pageSelect").empty();
-
-        var totalPages = Math.ceil(allBooks.length / resultsPerPage);
-
-        for (var i = 1; i <= totalPages; i++) {
-            $("#pageSelect").append(
-                "<option value='" + i + "'>" + i + "</option>"
-            );
-        }
+    if (searchTerm === "") {
+      alert("Please enter a search term.");
+      return;
     }
 
-    $("#pageSelect").change(function () {
-        var pageNumber = parseInt($(this).val());
-        displayPage(pageNumber);
-    });
+    $("#results").html("Loading...");
+    $("#pageSelect").empty();
 
-    function displayPage(pageNumber) {
+    // NOTE: maxResults allowed per request is 40
+    var apiUrl =
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+      encodeURIComponent(searchTerm) +
+      "&maxResults=40" +
+      "&key=" + apiKey;
 
-        $("#results").empty();
+    $.getJSON(apiUrl)
+      .done(function (data) {
 
-        var start = (pageNumber - 1) * resultsPerPage;
-        var end = start + resultsPerPage;
+        allBooks = data.items || [];
 
-        var booksToShow = allBooks.slice(start, end);
+        if (allBooks.length === 0) {
+          $("#results").html("No results found.");
+          return;
+        }
 
-        booksToShow.forEach(function (book) {
+        createPagination();
+        $("#pageSelect").val("1");
+        displayPage(1);
 
-            var title = book.volumeInfo.title || "No title";
-            var bookId = book.id;
+      })
+      .fail(function (xhr) {
+        $("#results").html("API error: " + xhr.status);
+      });
 
-            var image = "";
-            if (book.volumeInfo.imageLinks &&
-                book.volumeInfo.imageLinks.thumbnail) {
-                image = book.volumeInfo.imageLinks.thumbnail;
-            }
+  });
 
-            var bookHtml =
-                "<div>" +
-                "<a href='details.html?id=" + bookId + "'>" + title + "</a><br>" +
-                (image ? "<img src='" + image + "'>" : "") +
-                "</div><hr>";
+  $("#pageSelect").on("change", function () {
+    var pageNumber = parseInt($(this).val(), 10);
+    displayPage(pageNumber);
+  });
 
-            $("#results").append(bookHtml);
-        });
+  function createPagination() {
+    $("#pageSelect").empty();
+
+    var totalPages = Math.ceil(allBooks.length / resultsPerPage);
+
+    for (var i = 1; i <= totalPages; i++) {
+      $("#pageSelect").append("<option value='" + i + "'>" + i + "</option>");
     }
+  }
+
+  function displayPage(pageNumber) {
+
+    $("#results").empty();
+
+    var start = (pageNumber - 1) * resultsPerPage;
+    var end = start + resultsPerPage;
+
+    var booksToShow = allBooks.slice(start, end);
+
+    for (var i = 0; i < booksToShow.length; i++) {
+
+      var book = booksToShow[i];
+
+      var title = (book.volumeInfo && book.volumeInfo.title) ? book.volumeInfo.title : "No title";
+      var bookId = book.id;
+
+      var img = "";
+      if (book.volumeInfo &&
+          book.volumeInfo.imageLinks &&
+          book.volumeInfo.imageLinks.thumbnail) {
+        img = book.volumeInfo.imageLinks.thumbnail;
+      }
+
+      var html =
+        "<div>" +
+          "<a href='details.html?id=" + bookId + "'>" + title + "</a><br>" +
+          (img ? "<img src='" + img + "'>" : "") +
+        "</div><hr>";
+
+      $("#results").append(html);
+    }
+  }
 
 });
