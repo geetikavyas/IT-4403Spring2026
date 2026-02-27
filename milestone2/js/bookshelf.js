e5bbbda009c94731ea15b907e7a721651b506976
geetikavyas <168227711+geetikavyas@users.noreply.github.com>
2026-02-27 08:20:13 -0500

    Update bookshelf.js

--- milestone2/js/bookshelf.js
+++ milestone2/js/bookshelf.js
@@ -1,19 +1,76 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <title>My Bookshelf</title>
-</head>
-<body>
+console.log("bookshelf.js LOADED");
 
-  <h1>My Bookshelf</h1>
-  <p><a href="index.html">← Back to Search</a></p>
+$(document).ready(function () {
 
-  <div id="bookshelfResults">Loading...</div>
+  // Use the same API key you used in app.js
+  var apiKey = "AIzaSyAaXzebLvsKc4q66a4Ex0cux_FU-lrAalY";
 
-  <!-- jQuery first -->
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-  <!-- then your script -->
-  <script src="bookshelf.js"></script>
-</body>
-</html>
+  // From your Google Books shelf URL:
+  // https://books.google.com/books?uid=117733359037709170153&as_coll=0&...
+  var userId = "117733359037709170153";
+  var shelfId = "0";
+
+  // Where results will appear (must exist in bookshelf.html)
+  var $results = $("#bookshelfResults");
+
+  // Build the API URL (public shelf)
+  // Endpoint: GET /users/{userId}/bookshelves/{shelf}/volumes
+  var apiUrl =
+    "https://www.googleapis.com/books/v1/users/" +
+    encodeURIComponent(userId) +
+    "/bookshelves/" +
+    encodeURIComponent(shelfId) +
+    "/volumes?maxResults=40&key=" +
+    encodeURIComponent(apiKey);
+
+  $results.html("Loading bookshelf...");
+
+  $.getJSON(apiUrl)
+    .done(function (data) {
+
+      var items = data.items || [];
+
+      if (items.length === 0) {
+        $results.html(
+          "No books found in this shelf. Make sure the shelf is PUBLIC and has books."
+        );
+        return;
+      }
+
+      var html = "";
+
+      for (var i = 0; i < items.length; i++) {
+
+        var book = items[i];
+        var info = book.volumeInfo || {};
+
+        var title = info.title || "No title";
+        var authors = info.authors ? info.authors.join(", ") : "Not available";
+
+        var img = "";
+        if (info.imageLinks && info.imageLinks.thumbnail) {
+          img = info.imageLinks.thumbnail;
+        }
+
+        // Link each book to your Step-4 details page
+        var bookId = book.id;
+
+        html +=
+          "<div style='border:1px solid #ccc; padding:10px; margin-bottom:10px;'>" +
+            "<a href='details.html?id=" + encodeURIComponent(bookId) + "'>" +
+              "<strong>" + title + "</strong>" +
+            "</a><br>" +
+            "<span style='color:#555; font-size:13px;'>" + authors + "</span><br><br>" +
+            (img ? "<a href='details.html?id=" + encodeURIComponent(bookId) + "'><img src='" + img + "' style='max-width:150px;'></a>" : "") +
+          "</div>";
+      }
+
+      $results.html(html);
+
+    })
+    .fail(function (xhr) {
+      console.log("Bookshelf API error:", xhr.status, xhr.statusText, xhr.responseText);
+      $results.html("API error: " + xhr.status + " " + xhr.statusText);
+    });
+
+});
