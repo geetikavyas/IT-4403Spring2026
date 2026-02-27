47b5009b6eb5e5e8f385e7df013451eb51ae1c3d
geetikavyas <168227711+geetikavyas@users.noreply.github.com>
2026-02-27 08:38:33 -0500

    Update bookshelf.js

--- milestone2/js/bookshelf.js
+++ milestone2/js/bookshelf.js
@@ -1,77 +1,109 @@
-document.getElementById("bookshelfResults").innerHTML = "bookshelf.js is running...";
 console.log("bookshelf.js LOADED");
 
 $(document).ready(function () {
-
-  // Use the same API key you used in app.js
-  var apiKey = "AIzaSyDVf75MgxioDkvXiF5pq5JjPEJctEb9ONY";
-
-  // From your Google Books shelf URL:
-  // https://books.google.com/books?uid=117733359037709170153&as_coll=0&...
+  var apiKey = "AIzaSyAaXzebLvsKc4q66a4Ex0cux_FU-lrAalY";
   var userId = "117733359037709170153";
-  var shelfId = "0";
 
-  // Where results will appear (must exist in bookshelf.html)
   var $results = $("#bookshelfResults");
+  $results.html("Loading your public bookshelves...");
 
-  // Build the API URL (public shelf)
-  // Endpoint: GET /users/{userId}/bookshelves/{shelf}/volumes
-  var apiUrl =
+  // 1) Get list of PUBLIC bookshelves for this user
+  var listShelvesUrl =
     "https://www.googleapis.com/books/v1/users/" +
     encodeURIComponent(userId) +
-    "/bookshelves/" +
-    encodeURIComponent(shelfId) +
-    "/volumes?maxResults=40&key=" +
+    "/bookshelves?key=" +
     encodeURIComponent(apiKey);
 
-  $results.html("Loading bookshelf...");
-
-  $.getJSON(apiUrl)
+  $.getJSON(listShelvesUrl)
     .done(function (data) {
+      var shelves = data.items || [];
 
-      var items = data.items || [];
-
-      if (items.length === 0) {
+      if (shelves.length === 0) {
         $results.html(
-          "No books found in this shelf. Make sure the shelf is PUBLIC and has books."
+          "No public bookshelves found. Make sure your bookshelf is set to PUBLIC in Google Books."
         );
         return;
       }
 
-      var html = "";
+      // Pick a shelf automatically:
+      // Prefer a custom shelf (not the built-in ones) if available,
+      // otherwise just use the first one returned.
+      var chosenShelf = null;
 
-      for (var i = 0; i < items.length; i++) {
-
-        var book = items[i];
-        var info = book.volumeInfo || {};
-
-        var title = info.title || "No title";
-        var authors = info.authors ? info.authors.join(", ") : "Not available";
-
-        var img = "";
-        if (info.imageLinks && info.imageLinks.thumbnail) {
-          img = info.imageLinks.thumbnail;
+      // Try to find any shelf with a title and volumeCount > 0
+      for (var i = 0; i < shelves.length; i++) {
+        if ((shelves[i].volumeCount || 0) > 0) {
+          chosenShelf = shelves[i];
+          break;
         }
-
-        // Link each book to your Step-4 details page
-        var bookId = book.id;
-
-        html +=
-          "<div style='border:1px solid #ccc; padding:10px; margin-bottom:10px;'>" +
-            "<a href='details.html?id=" + encodeURIComponent(bookId) + "'>" +
-              "<strong>" + title + "</strong>" +
-            "</a><br>" +
-            "<span style='color:#555; font-size:13px;'>" + authors + "</span><br><br>" +
-            (img ? "<a href='details.html?id=" + encodeURIComponent(bookId) + "'><img src='" + img + "' style='max-width:150px;'></a>" : "") +
-          "</div>";
       }
+      if (!chosenShelf) chosenShelf = shelves[0];
 
-      $results.html(html);
+      var shelfId = chosenShelf.id;
+      var shelfTitle = chosenShelf.title || "My Bookshelf";
 
+      $results.html("Loading books from: <strong>" + shelfTitle + "</strong> ...");
+
+      // 2) Now load volumes from that shelf
+      var volumesUrl =
+        "https://www.googleapis.com/books/v1/users/" +
+        encodeURIComponent(userId) +
+        "/bookshelves/" +
+        encodeURIComponent(shelfId) +
+        "/volumes?maxResults=40&key=" +
+        encodeURIComponent(apiKey);
+
+      $.getJSON(volumesUrl)
+        .done(function (volData) {
+          var items = volData.items || [];
+
+          if (items.length === 0) {
+            $results.html(
+              "Shelf is public but has no books (or API can't see them). Add books to this public shelf and refresh."
+            );
+            return;
+          }
+
+          var html = "<h2>" + shelfTitle + "</h2>";
+
+          for (var j = 0; j < items.length; j++) {
+            var book = items[j];
+            var info = book.volumeInfo || {};
+
+            var title = info.title || "No title";
+            var authors = info.authors ? info.authors.join(", ") : "Not available";
+
+            var img = "";
+            if (info.imageLinks && info.imageLinks.thumbnail) {
+              img = info.imageLinks.thumbnail;
+            }
+
+            var bookId = book.id;
+
+            html +=
+              "<div style='border:1px solid #ccc; padding:10px; margin-bottom:10px;'>" +
+                "<a href='details.html?id=" + encodeURIComponent(bookId) + "'>" +
+                  "<strong>" + title + "</strong>" +
+                "</a><br>" +
+                "<span style='color:#555; font-size:13px;'>" + authors + "</span><br><br>" +
+                (img
+                  ? "<a href='details.html?id=" + encodeURIComponent(bookId) + "'>" +
+                      "<img src='" + img + "' style='max-width:150px;'>" +
+                    "</a>"
+                  : "") +
+              "</div>";
+
+          }
+
+          $results.html(html);
+        })
+        .fail(function (xhr) {
+          console.log("Volumes API error:", xhr.status, xhr.statusText, xhr.responseText);
+          $results.html("API error (volumes): " + xhr.status + " " + xhr.statusText);
+        });
     })
     .fail(function (xhr) {
-      console.log("Bookshelf API error:", xhr.status, xhr.statusText, xhr.responseText);
-      $results.html("API error: " + xhr.status + " " + xhr.statusText);
+      console.log("Shelves API error:", xhr.status, xhr.statusText, xhr.responseText);
+      $results.html("API error (bookshelves list): " + xhr.status + " " + xhr.statusText);
     });
-
 });
